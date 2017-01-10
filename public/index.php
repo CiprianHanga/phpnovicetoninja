94851284406964b225d156a6733fb6103f82065b
Ciprian Hanga <ciprianhanga@gmail.com>
2017-01-10 22:35:01 +0200

    working on a new solution

--- public/index.php
+++ public/index.php
@@ -1,6 +1,19 @@
 <?php
 echo '<cite>Premature optimization is the root of all evil.</cite> --Donald Knuth<br>';
 
+if (!isset($_POST['firstname'])) {
+    include 'login_form.html.php';
+} else {
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $password = $_POST['password'];
 
+    if ($firstname=='Ciprian' and $lastname=='Hanga' and $password=='secret') {
+        echo 'OK';
+    } else {
+        include 'login_form.html.php';
+    }
+
+}
 
 ?>
