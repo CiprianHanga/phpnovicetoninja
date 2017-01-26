b6fd3e7074fb1b2723ca2f146d599ffb843c1e2a
Ciprian Hanga <ciprianhanga@gmail.com>
2017-01-26 17:10:05 +0200

    More testing.

--- public/_code-fu/testing_included_files/test.php
+++ public/_code-fu/testing_included_files/test.php
@@ -1,10 +1,13 @@
 <?php
+// Function to calculate the area of a square.
+// Comment/uncomment the include instruction to see values change.
 
 function area($width, $height) {
     return $width * $height;
 }
 
 $area = area(3, 5);
+
 include 'test2.php';
 
 echo $area;
--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include $_SERVER['DOCUMENT_ROOT'] . '/includes/magicquotes.php';
-include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.html.php';
-
-include $_SERVER['DOCUMENT_ROOT'] . '/test.php';
-
-include $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.html.php';
-?>
