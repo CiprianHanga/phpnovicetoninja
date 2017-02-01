1b6e212ac48b1d5077d726e342a274bbd79c44d9
Ciprian Hanga <ciprianhanga@gmail.com>
2017-02-01 04:13:07 +0200

    First class tests.

---
+++ public/test/demo.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * Ciprian Hanga (c) 2017 (ciprianhanga@gmail.com)
+ * Title: 
+ * Description: 
+ */
+
+require 'class.Address.php';
+
+echo '<h2>Instantiating Address</h2>';
+$address = new Address;
+
+echo '<h2>Empty Address</h2>';
+echo '<tt><pre>' . var_export($address, TRUE) . '</pre></tt>';
+
+echo '<h2>Setting Properties...</h2>';
+$address->street_address_1 = '555 Fake Street';
+$address->city_name = 'Townsville';
+$address->subdivision_name = 'State';
+$address->postal_code = '12345';
+$address->country_name = 'United Kingdom';
+echo '<tt><pre>' . var_export($address, TRUE) . '</pre></tt>';
+
+echo '<h2>Displaying address...</h2>';
+echo $address->display();
