4682c2e10a9e785e8ed528dd08b1c353370678da
Ciprian Hanga <ciprianhanga@gmail.com>
2017-02-03 00:01:18 +0200

    Testing protected properties.

--- public/test/index.php
+++ public/test/index.php
@@ -1,3 +1,20 @@
 <?php
 
-require 'demo.php';
+class Person {
+    function say_hello() {
+        echo 'hello from inside the class';
+    }
+}
+
+$person1 = new Person();
+$person2 = new Person();
+
+echo get_class($person1) . '<br>';
+
+if (is_a($person, 'Person')) {
+    echo 'OK';
+} else {
+    echo 'NOT OK';
+}
+
+$person->say_hello();
